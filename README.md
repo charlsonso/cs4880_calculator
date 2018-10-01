ec580eca12d803af553e46a6fe20cc87ad4640b9
so0p <charlsonsoo@gmail.com>
2018-10-01 14:08:22 -0700

    working prototype

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 #Calculator
 
 Calculator created in pyQt 5 for CS4800 at California Polytechnical University: Pomona
+
+# Known Bugs
+* display does not show past 5 values
+* no catch for invalid expressions
--- calculator.py
+++ calculator.py
@@ -4,58 +4,56 @@ from PyQt5.QtWidgets import (QApplication,
                              QWidget,
                              QPushButton,
                              QLabel)
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import * 
+from PyQt5.QtCore import *
 
-
-# create buttons
-# create display
-# create stuff
-
-def add(x,y):
-    return x + y
-
-def subtract(x,y):
-    return x - y
-
-def multiply(x,y):
-    return x * y
-
-def divide(x,y):
-    return x/y
 
 '''
-class calculate():
-    self.first = ""
-        type: string
-    self.second
-        type: string
-    self.operator
-
-    def press an operator
-        save into self.operator
-        clear out screen
-    
-    def input_number
-        if self.first == "":
-        elif self.second == "":
-        else:
-            clear second number
-            input into second number
-
-    def clear
-    def input
-
+how to figure out to display updated text?
+when eval erors, send invalid text to text box
 '''
-display_value = "hi"
 class App(QWidget):
     def __init__(self):
         super().__init__()
         self.title = 'Calculator'
         self.left = 10
         self.top  = 10
-        self.width = 250 
+        self.width = 310
         self.height = 310
+
+        self.equation=""
+        self.display=""
+        self.display_box = QLabel(self)
+        self.display_box.move(10, 10)
+        self.display_box.resize(100, 10)
         self.initUI()
+
+    def update_display_box(self):
+        if self.display == "":
+            self.display_box.setText(self.equation)
+        else:
+            self.display_box.setText(self.display)
+
+
+    def add_number_to_equation(self, value):
+        self.equation = self.equation + value
+        print(self.equation)
+        self.update_display_box()
+
+    def solve(self):
+        self.display = str(eval(self.equation))
+        print(self.display)
+        self.equation = ""
+        self.update_display_box()
+        self.clear_equation()
+
+    def clear_equation(self):
+        self.equation=""
+        self.display=""
+
+    def clear_display(self):
+        self.clear_equation()
+        self.update_display_box()
 
     def initUI(self):
         self.setWindowTitle(self.title)
@@ -63,98 +61,106 @@ class App(QWidget):
                          self.top,
                          self.width,
                          self.height)
+        
+        calculator_button_title = ()
 
+        # Generate 9 buttons
+        # 
+        # Add button titles '0-9'
+#        for i in range(0,10):
+#            calculator_button_title.append(str(i))
+        # Add . C Symbols
+#            calculator_button_title.append(('.', 'C')
+
+
+        
         self.nine = QPushButton('9', self)
-#        self.one.clicked.connect(print(1))
+        self.nine.clicked.connect(lambda: self.add_number_to_equation(value = '9'))
         self.nine.resize(50, 50)
         self.nine.move(130, 70)
 
-
         self.eight = QPushButton('8', self)
-#        self.one.clicked.connect(print(1))
+        self.eight.clicked.connect(lambda: self.add_number_to_equation(value = '8'))
         self.eight.resize(50, 50)
         self.eight.move(70, 70)
 
         self.seven = QPushButton('7', self)
-#        self.one.clicked.connect(print(1))
+        self.seven.clicked.connect(lambda: self.add_number_to_equation(value = '7'))
         self.seven.resize(50, 50)
         self.seven.move(10, 70)
  
         self.six = QPushButton('6', self)
-#        self.one.clicked.connect(print(1))
+        self.six.clicked.connect(lambda: self.add_number_to_equation(value = '6'))
         self.six.resize(50, 50)
         self.six.move(130, 130)
  
         self.five = QPushButton('5', self)
-#        self.one.clicked.connect(print(1))
+        self.five.clicked.connect(lambda: self.add_number_to_equation(value = '5'))
         self.five.resize(50, 50)
         self.five.move(70, 130)
  
         self.four = QPushButton('4', self)
-#        self.one.clicked.connect(print(1))
+        self.four.clicked.connect(lambda: self.add_number_to_equation(value = '4'))
         self.four.resize(50, 50)
         self.four.move(10, 130)
  
         self.three = QPushButton('3', self)
-#        self.one.clicked.connect(print(1))
+        self.three.clicked.connect(lambda:self.add_number_to_equation(value='3'))
         self.three.resize(50, 50)
         self.three.move(130, 190)
 
         self.two = QPushButton('2', self)
-#        self.one.clicked.connect(print(1))
+        self.two.clicked.connect(lambda:self.add_number_to_equation(value='2'))
         self.two.resize(50, 50)
         self.two.move(70, 190)
  
         self.one = QPushButton('1', self)
-#        self.one.clicked.connect(print(1))
+        self.one.clicked.connect(lambda:self.add_number_to_equation(value='1'))
         self.one.resize(50, 50)
         self.one.move(10, 190)
  
         self.zero = QPushButton('0', self)
-#        self.one.clicked.connect(print(1))
+        self.zero.clicked.connect(lambda:self.add_number_to_equation(value='0'))
         self.zero.resize(50, 50)
         self.zero.move(10, 250)
  
         self.period = QPushButton('.', self)
-#        self.one.clicked.connect(print(1))
+        self.period.clicked.connect(lambda:self.add_number_to_equation(value='.'))
         self.period.resize(50, 50)
         self.period.move(70, 250)
  
         self.clear = QPushButton('C', self)
-#        self.one.clicked.connect(print(1))
+        self.clear.clicked.connect(self.clear_display)
         self.clear.resize(50, 50)
         self.clear.move(130, 250)
 
         self.plus = QPushButton('+', self)
-#        self.one.clicked.connect(print(1))
+        self.plus.clicked.connect(lambda: self.add_number_to_equation(value='+'))
         self.plus.resize(50, 50)
         self.plus.move(190, 70)
 
         self.minus = QPushButton('-', self)
-#        self.one.clicked.connect(print(1))
+        self.minus.clicked.connect(lambda: self.add_number_to_equation(value='-'))
         self.minus.resize(50, 50)
         self.minus.move(190, 130)
 
         self.times = QPushButton('*', self)
-#        self.one.clicked.connect(print(1))
+        self.times.clicked.connect(lambda: self.add_number_to_equation(value='*'))
         self.times.resize(50, 50)
         self.times.move(190, 190)
 
         self.division = QPushButton('/', self)
-#        self.one.clicked.connect(print(1))
+        self.division.clicked.connect(lambda: self.add_number_to_equation(value='-'))
         self.division.resize(50, 50)
         self.division.move(190, 250)
         
-        self.create_display_box()
+        self.equal = QPushButton('=', self)
+        self.equal.clicked.connect(self.solve)
+        self.equal.resize(50, 50)
+        self.equal.move(250, 250)
+
+        self.update_display_box()
         self.show()
-
-    def create_display_box(self):
-        self.display_box = QLabel(self)
-        self.display_box.setText(display_value)
-
-def main():
-    while(1):
-        time.sleep(1)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
